ea49409d776c9f9cd7ddaeec56c33f29ce645595
TuanDo <anhtuan7194@gmail.com>
2024-01-04 09:52:20 +0700

    Update port for deploy to render

--- index.js
+++ index.js
@@ -93,6 +93,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to request on port 8000');
+const port = process.env.PORT || 8000;
+
+server.listen(port, '127.0.0.1', () => {
+  console.log(`Listening to request on port ${port}`);
 });
